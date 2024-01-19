7dfa17ece06da3c38782e073f4de086315ae841d
Bhoma Ram <120722224+Bhoma-RamDaiya@users.noreply.github.com>
2024-01-19 15:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+// function hasKey(key) {
+//   //   write your code here
+// }
+
+// // Do not change the code below
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
+// Global object
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
+// Function to check if the key exists in the global object
 function hasKey(key) {
-  //   write your code here
+    // Check if the key exists in the sampleObject
+    return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Examples
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
+
